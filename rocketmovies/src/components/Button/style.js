73739833a67e8d5605edebceeca8f73eb2f79e93
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-09-18 18:17:47 -0300

    button component changed.

--- rocketmovies/src/components/Button/style.js
+++ rocketmovies/src/components/Button/style.js
@@ -4,9 +4,17 @@ export const Container = styled.button`
   width: 100%;
   height: 56px;
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   background-color: ${({ theme }) => theme.COLORS.ACCENT};
   border: none;
   border-radius: 4px;
   color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
   outline: none;
+
+  svg {
+    margin-right: 5px;
+  }
 `;
