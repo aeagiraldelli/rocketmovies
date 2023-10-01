993869ca700988ec577ab4e582330b050f69a941
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-10-01 12:01:44 -0300

    signup page working

---
+++ rocketmovies/src/hooks/auth.jsx
@@ -0,0 +1,92 @@
+import { createContext, useState, useEffect, useContext } from 'react';
+
+import { api } from '../services/api';
+
+const LOCAL_USER_KEY = '@rocketmovies:user';
+const LOCAL_TOKEN_KEY = '@rocketmovies:token';
+const AuthContext = createContext({});
+
+export function AuthProvider({ children }) {
+  const [data, setData] = useState({});
+
+  async function login({ email, password }) {
+    try {
+      const response = await api.post('/session', { email, password });
+      const { user, token } = response.data;
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+      setData({ user, token });
+
+      localStorage.setItem(LOCAL_USER_KEY, JSON.stringify(user));
+      localStorage.setItem(LOCAL_TOKEN_KEY, token);
+    } catch (error) {
+      if (error.response) {
+        return alert(error.response.data.message);
+      } else {
+        return alert(
+          'Erro ao tentar realizar login. Tente novamente mais tarde.'
+        );
+      }
+    }
+  }
+
+  function logout() {
+    localStorage.removeItem(LOCAL_TOKEN_KEY);
+    localStorage.removeItem(LOCAL_USER_KEY);
+    setData({});
+  }
+
+  async function updateProfile({ user, avatarFile }) {
+    try {
+      if (avatarFile) {
+        const fileUploadForm = new FormData();
+        fileUploadForm.append('avatar', fileUploadForm);
+
+        const response = await api.patch('/users/avatar', fileUploadForm);
+        user.avatar = response.data.avatar;
+      }
+
+      const response = await api.put('/users', user);
+      localStorage.setItem(LOCAL_USER_KEY, JSON.stringify(response.data.user));
+      setData({
+        user: response.data.user,
+        token: (prevState) => prevState.token,
+      });
+      return alert('Dados do usuário atualizados com sucesso.');
+    } catch (error) {
+      if (error.response) {
+        return alert(error.response.data.message);
+      } else {
+        return alert(
+          'Erro ao tentar atualizar o perfil do usuário. Tente novamente mais tarde.'
+        );
+      }
+    }
+  }
+
+  useEffect(() => {
+    const user = localStorage.getItem(LOCAL_USER_KEY);
+    const token = localStorage.getItem(LOCAL_TOKEN_KEY);
+
+    if (token && user) {
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      setData({
+        user: JSON.parse(user),
+        token,
+      });
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ user: data.user, updateProfile, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  return context;
+}
--- rocketmovies/src/pages/SignUp/index.jsx
+++ rocketmovies/src/pages/SignUp/index.jsx
@@ -1,13 +1,47 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { HiArrowLeft, HiMail, HiLockClosed, HiUser } from 'react-icons/hi';
 
-import { Background, Container, Form } from './style';
+import { api } from '../../services/api';
 
 import { BigTitle } from '../../components/BigTitle';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import { Link } from 'react-router-dom';
 
+import { Background, Container, Form } from './style';
+
 export function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navitate = useNavigate();
+
+  function handleSignUpClick() {
+    if (!name || !email || !password) {
+      return alert('Por favor, preencha todos os campos.');
+    }
+
+    console.debug(name, email, password);
+
+    api
+      .post('/users', { name, email, password })
+      .then(() => {
+        alert('Cadastro realizado com sucesso.');
+        navitate('/');
+      })
+      .catch((error) => {
+        if (error.response) {
+          return alert(error.response.data.message);
+        } else {
+          return alert(
+            'Não foi possível realizar o cadastro. Tente novamente mais tarde.'
+          );
+        }
+      });
+  }
+
   return (
     <Container>
       <Form>
@@ -15,16 +49,33 @@ export function SignUp() {
         <p>Faça um registro de tudo que você já assistiu.</p>
         <h2>Crie sua conta</h2>
         <div className="input-group">
-          <Input icon={HiUser} placeholder="Nome" type="text" required />
-          <Input icon={HiMail} placeholder="E-mail" type="email" required />
+          <Input
+            icon={HiUser}
+            placeholder="Nome"
+            type="text"
+            required
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Input
+            icon={HiMail}
+            placeholder="E-mail"
+            type="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <Input
             icon={HiLockClosed}
             placeholder="Senha"
             type="password"
             required
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <Button className="btn-login" title="Cadastrar" />
+        <Button
+          className="btn-login"
+          title="Cadastrar"
+          onClick={handleSignUpClick}
+        />
         <Link className="link" to="/">
           <HiArrowLeft size={20} />
           Voltar para a tela de login
